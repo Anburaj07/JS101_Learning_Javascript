921230020309e9296116e147a27032bcd0dfe880
Anburaj s <anburaj2203@gmail.com>
2023-01-30 15:30:36 +0530

    day1

---
+++ Problem2.js
@@ -0,0 +1,6 @@
+let name="Anburaj";
+console.log(name);
+name="Selvaraj";
+console.log(name);
+name="Gnana Soundari";
+console.log(name);
---
+++ problem4.js
@@ -0,0 +1,22 @@
+const name="STUDENT NAME: Anbu";
+let school="      N.V.S Hr.Sec.School";
+let sec="SECTION     : D";
+const rollno="ROLL NO     : 221"
+let mark1=95,mark2=85,mark3=75;
+let grade1="     A",grade2="    B",grade3="   C";
+let space=""
+console.log("✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿");
+console.log("          REPORT CARD");
+console.log(school);
+console.log("✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿ ✿");
+console.log(space);
+console.log(name);
+console.log(rollno);
+console.log(sec);
+console.log(" _____________________________________");
+console.log("|SUBJECT   GRADE  MARKS               |");
+console.log("|_____________________________________|");
+console.log("|","Maths",grade1,space,space,space,space,mark1,"                |");
+console.log("|","Sciene",grade2,space,space,space,space,mark2,"                |");
+console.log("|","English",grade3,space,space,space,space,mark3,"                |");
+console.log("|_____________________________________|")
