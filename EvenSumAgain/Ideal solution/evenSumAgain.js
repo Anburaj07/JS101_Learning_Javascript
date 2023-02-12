99aa87eec18a3ccf1da607dd4de143c06b9d291b
Anburaj s <anburaj2203@gmail.com>
2023-02-12 19:55:08 +0530

    Updated

--- EvenSumAgain/Ideal solution/evenSumAgain.js
+++ EvenSumAgain/Ideal solution/evenSumAgain.js
@@ -1,12 +1,8 @@
- for(let i=1;i<=4;i++){
-        let s="";
-   let sum=0;
-        for(let j=1;j<=i;j++){
-            s+=j;
-          for(let z=0;z<s.length;z++){
-            if(s[z]%2==0){
-              sum=sum+z;
+ for(let i=1;i<=num;i++){
+        let sum=0;
+        for(j=1;j<=i;j++){
+            if(j%2==0){
+                sum+=j;
             }
-          }
         }console.log(sum);
     }
